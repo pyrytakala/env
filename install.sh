e2353b1a42718ecbc883a3819e48e0b806ca3dca
Pyry Takala <pyrytakala@users.noreply.github.com>
2017-03-22 14:07:15 +0000

    Create install.sh

---
+++ install.sh
@@ -0,0 +1,6 @@
+# !/bin/bash
+URL='https://raw.githubusercontent.com/pyrytakala/env/master/'
+curl -sS ${URL}.bashrc >> ~/.bashrc
+curl -sS ${URL}.vimrc >> ~/.vimrc
+curl -sS ${URL}.tmux.conf > ~/.tmux.conf
+curl -sS ${URL}.ideavimrc > ~/.ideavimrc
