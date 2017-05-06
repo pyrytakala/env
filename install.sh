22a38cdcaa30defc10164048306a643163d0deaf
Pyry Takala <pyrytakala@users.noreply.github.com>
2017-05-06 13:30:54 +0100

    Update install.sh

--- install.sh
+++ install.sh
@@ -4,3 +4,4 @@ curl -sS ${URL}.bashrc >> ~/.bashrc
 curl -sS ${URL}.vimrc >> ~/.vimrc
 curl -sS ${URL}.tmux.conf > ~/.tmux.conf
 curl -sS ${URL}.ideavimrc > ~/.ideavimrc
+curl -sS ${URL}.gitignore_global > ~/.gitignore_global
