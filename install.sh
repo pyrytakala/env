794348fde16caad368dcc80a0adeeafa1520b00c
Pyry <pyry.takala@gmail.com>
2017-06-01 11:48:31 +0100

    added bashrc sourcing for mac

--- install.sh
+++ install.sh
@@ -2,6 +2,7 @@
 URL='https://raw.githubusercontent.com/pyrytakala/env/master/'
 curl -sS ${URL}.bashrc >> ~/.bashrc
 curl -sS ${URL}.vimrc >> ~/.vimrc
+curl -sS ${URL}.profile >> ~/.profile
 curl -sS ${URL}.tmux.conf > ~/.tmux.conf
 curl -sS ${URL}.ideavimrc > ~/.ideavimrc
 curl -sS ${URL}.gitignore_global > ~/.gitignore_global
